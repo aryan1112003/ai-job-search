5950dadbe74166d06d24e3edff8f3c501241e4a3
aryan1112003 <67147314+aryan1112003@users.noreply.github.com>
2025-01-02 16:38:37 +0530

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  GEMINI_API_KEY: 'AIzaSyA1DJxG5PPVvR1oo2p5Cfdy-yFsKVZyQJU',
-  GOOGLE_JOBS_API_KEY: 'AIzaSyA1DJxG5PPVvR1oo2p5Cfdy-yFsKVZyQJU',
+  GEMINI_API_KEY: 'api here',
+  GOOGLE_JOBS_API_KEY: 'api here',
   GOOGLE_JOBS_API_URL: 'https://jobs.googleapis.com/v4/jobs:search',
   MODEL_NAME: 'gemini-pro',
   MAX_JOBS: 10
